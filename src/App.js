be89640e30889f9fd70e820514b3bc2a00f73631
simonhuwiler <webmaster@simonhuwiler.ch>
2020-02-28 18:36:49 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>BrokJSON</h1>
+        <h2>The space saving GeoJSON-alternative</h2>
+      </header>
+      <nav>
+        Introduction | Converter | Install | Specification
+      </nav>
+    sdfdfs
+    </div>
+  );
+}
+
+export default App;
